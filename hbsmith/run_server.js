49928841f7ea70e7bc178afc53843925ea5a43f0
Jesang Yoon <yoonjs2@gmail.com>
2025-02-18 15:23:30 +0900

    fix

--- hbsmith/run_server.js
+++ hbsmith/run_server.js
@@ -72,6 +72,6 @@ const server = http.createServer(async (req, res) => {
 });
 
 const PORT = process.env.PORT || 3000;
-server.listen(PORT, () => {
-    console.log(`Server running on port ${PORT}`);
+server.listen(PORT, '0.0.0.0', () => {
+    console.log(`Server running on http://0.0.0.0:${PORT}`);
 });
